75e252c0176a7cff29f442a737775e0887c1a1fa
maddrum <maddrum9@gmail.com>
2018-06-16 11:12:27 +0300

    test migrations

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class TestModel(models.Model)
+class TestModel(models.Model):
     test = models.CharField(max_length=150)
     test1 = models.IntegerField()
